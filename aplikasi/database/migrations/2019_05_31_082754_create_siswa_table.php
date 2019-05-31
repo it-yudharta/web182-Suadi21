2673bb71706e0693e107364719ee6b6ba260d1d5
SUADI <50537303+Suadi21@users.noreply.github.com>
2019-05-31 15:41:01 +0700

    master dari composer

---
+++ aplikasi/database/migrations/2019_05_31_082754_create_siswa_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama');
+            $table->string('kelas');
+            $table->string('jurusan');
+            $table->string('nama_alat');
+            $table->string('jam_pinjam');
+            $table->string('jam_kembali');
+            $table->text('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswa');
+    }
+}
