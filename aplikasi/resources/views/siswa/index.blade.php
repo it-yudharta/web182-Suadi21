0b080dbe3398d41ed6a186c958a2abd91bc94c93
SUADI <50537303+Suadi21@users.noreply.github.com>
2019-06-23 11:40:32 +0700

    notif simpan dan update

--- aplikasi/resources/views/siswa/index.blade.php
+++ aplikasi/resources/views/siswa/index.blade.php
@@ -117,7 +117,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary onclick="return confirm('Data Berhasil di simpan')">Submit</button>
                 </form>
             </div>
             </div>
